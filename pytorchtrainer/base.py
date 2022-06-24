abe28e621159852a505cd7b8bc37d4a110557c2a
AshwinSakhare <asakhare@zdatainc.com>
2022-06-24 12:09:55 -0400

    update base

--- pytorchtrainer/base.py
+++ pytorchtrainer/base.py
@@ -3,13 +3,7 @@ import sys
 import time
 
 import torch
-from torch.utils.data import Dataset, DataLoader
-
-import torch.distributed as dist
-import torch.multiprocessing as mp
-
-from torch.utils.data.distributed import DistributedSampler
-from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.tensorboard import SummaryWriter
 
 from typing import List, Optional, Iterable
 
@@ -22,14 +16,8 @@ class BaseTrainer:
         net: torch.nn.Module = None,
 
         # data / dataloaders
-        train_dataset: Optional[Dataset] = None,
-        valid_dataset: Optional[Dataset] = None,
-
-        train_loader: Optional[DataLoader] = None,
-        valid_loader: Optional[DataLoader] = None,
-
-        batch_size: Optional[int] = None,
-        num_workers: Optional[int] = None,
+        train_loader: Iterable = None,
+        valid_loader: Optional[Iterable] = None,
 
         # loss functions
         crit: List[torch.nn.Module] = None,
@@ -46,15 +34,14 @@ class BaseTrainer:
         mixed_precision: bool = False,
 
         # accelerator
-        device_ids: List[int] = None,
-        ddp: bool = False,
+        device: torch.device = None,
 
         # artifact saving
-        checkpoint_every: int = 100,
+        checkpoint_every: int = 10,
         checkpoint_dir: Optional[str] = './',
         model_name: Optional[str] = 'model',
 
-        tb_writer = None,
+        tb_writer: SummaryWriter = None,
     ):
         '''
         Initializes the trainer.
@@ -97,11 +84,8 @@ class BaseTrainer:
             mixed_precision: bool
                 Whether to use mixed precision training or not
 
-            device_ids: List[int]
-                List of device ids to use for training
-
-            ddp: bool
-                Whether to use distributed data parallel training
+            device: torch.device
+                Whether to use any supported accelerator, for example a GPU
 
             checkpoint_every: int
                 How often to save model weights
@@ -126,8 +110,6 @@ class BaseTrainer:
             metric_tracking: dict
                 trackes the metric values for each batch
         '''
-        self.train_dataset = train_dataset
-        self.valid_dataset = valid_dataset
         self.train_loader = train_loader
         self.valid_loader = valid_loader
 
@@ -137,13 +119,10 @@ class BaseTrainer:
             self.crit_lambdas = [1. for _ in range(len(self.crit))]
         else:
             self.crit_lambdas = crit_lambdas
-        self.batch_size = batch_size
-        self.num_workers = num_workers
 
         self.metrics = metrics
 
-        self.ddp = ddp
-        self.device_ids = device_ids
+        self.device = device
         self.optimizer = optimizer
         self.scheduler = scheduler
 
@@ -161,30 +140,15 @@ class BaseTrainer:
         # loss and metrics tracking
         self.losses = {'train': [], 'valid': []}
         self.metric_names = metric_names
-        if self.metric_names is None and metrics is not None:
+        if self.metric_names is None:
             self.metric_names = [m.__name__.capitalize()  for m in metrics]
         if metrics is not None:
             self.metric_tracking = {mn: {'train': [], 'valid': []} for mn in self.metric_names}
 
         self.tb_writer = tb_writer
 
-        if ddp:
-            # initialize distributed data parallel modules
-            print('DDP setup ...')
-            # set up the processing group
-            self.init_ddp()
-            self.world_size = len(self.device_ids)
-        else:
-            self.device = torch.device('cuda:{}'.format(device_ids[0]) if torch.cuda.is_available() else 'cpu')
 
-    def fit(self):
-        if self.ddp:
-            # first input to this is automatically rank or device
-            mp.spawn(self.train_network, args=(self.world_size,), nprocs = self.world_size, join=True)
-        else:
-            self.train_network()
-
-    def train_network(self, device = None, world_size = None):
+    def train_network(self):
         '''
         Train the network using the train and valid functions.
 
@@ -192,14 +156,6 @@ class BaseTrainer:
 
         Allows keyboard interrupt to stop training at any time
         '''
-        if self.ddp:
-            dist.init_process_group("gloo", rank = device, world_size = world_size)
-            net, optimizer = self.setup_ddp_modules(device, self.net)
-        else:
-            device = self.device
-            net = self.net.to(device)
-            optimizer = self.optimizer
-
         try:
             print()
             print('-----------------------------------------')
@@ -209,21 +165,18 @@ class BaseTrainer:
 
             for e in range(self.epochs):
 
-                if (self.ddp and device == 0) or (not self.ddp):
-                    print('\n' + 'Iter {}/{}'.format(e + 1, self.epochs))
-
+                print('\n' + 'Iter {}/{}'.format(e + 1, self.epochs))
                 start = time.time()
-                self.run_epoch(net, optimizer, device, mode = 'train')
+                self.run_epoch(mode='train', e=e)
 
                 if self.valid_loader is not None:
                     with torch.no_grad():
-                        self.run_epoch(net, optimizer, device, mode = 'valid')
+                        self.run_epoch(mode='valid', e=e)
 
                 if self.scheduler is not None:
                     self.scheduler.step()
 
-                if (self.ddp and device == 0) or (not self.ddp):
-                    print('Time: {}'.format(time.time() - start))
+                print('Time: {}'.format(time.time() - start))
 
                 if e % self.checkpoint_every == 0:
                     model_path = os.path.join(self.checkpoint_dir, self.model_name)
@@ -233,10 +186,7 @@ class BaseTrainer:
         except KeyboardInterrupt:
             pass
 
-        if self.ddp:
-            dist.destroy_process_group()
-
-    def run_epoch(self, net, optimizer, device, mode: str):
+    def run_epoch(self, mode: str, e: int):
         '''
         Uses the data loader to grab a batch of data
 
@@ -262,11 +212,11 @@ class BaseTrainer:
             n_batches = len(self.valid_loader)
 
         for i, (inputs, targets) in iterator:
-            inputs, targets = inputs.to(device), targets.to(device)
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
 
             with torch.cuda.amp.autocast(enabled=self.mixed_precision):
                 # get predictions
-                preds = net(inputs)
+                preds = self.net(inputs)
 
                 loss = self.get_loss(preds, targets)
                 running_loss += loss.item()
@@ -276,7 +226,7 @@ class BaseTrainer:
                 self.scaler.scale(loss).backward()
 
                 # update weights
-                self.scaler.step(optimizer)
+                self.scaler.step(self.optimizer)
                 self.scaler.update()
                 # zero gradients for next run
                 self.net.zero_grad(set_to_none=True)
@@ -285,104 +235,49 @@ class BaseTrainer:
 
             # track metrics
             if self.metrics is not None:
-
                 metric_vals = [m(targets, preds).item() for m in self.metrics]
                 for m_idx, mv in enumerate(metric_vals):
                     self.metric_tracking[self.metric_names[m_idx]][mode].append(mv)
                     running_metrics[m_idx] += mv
 
-            if self.tb_writer is not None:
-                self.writer.add_scalar(f'Loss/{mode}', loss.item(), self.iterations)
-
-                for metric_name in self.metric_tracking:
-                    self.writer.add_scalaer(f'{metric_name}/{mode}', self.metric_tracking[metric_name][mode][-1], self.iterations)
-
-            if (self.ddp and device == 0) or (not self.ddp):
-                # display running statistics
-                sys.stdout.write('\r')
-                sys.stdout.write('{} B: {:>3}/{:<3} | Loss: {:.4}'.format(
-                    mode.capitalize(),
-                    i+1,
-                    n_batches,
-                    loss.item(),
-                ))
-                if self.metrics is not None:
-                    for m_idx, mn in enumerate(self.metric_names):
-                        sys.stdout.write(' {}: {:.4}'.format(mn, metric_vals[m_idx]))
-                sys.stdout.flush()
-
-        if (self.ddp and device == 0) or (not self.ddp):
-            # display batch statistics
-            print(
-                '\n' + 'Avg Loss: {:.4}'.format(
-                    running_loss / n_batches,
-                )
-            )
-
+            # display running statistics
+            sys.stdout.write('\r')
+            sys.stdout.write('{} B: {:>3}/{:<3} | Loss: {:.4}'.format(
+                mode.capitalize(),
+                i+1,
+                n_batches,
+                loss.item(),
+            ))
             if self.metrics is not None:
-                for m_idx, rm in enumerate(running_metrics):
-                    print('Avg {}: {:.4}'.format(
-                        self.metric_names[m_idx], rm / n_batches
-                    ))
+                for m_idx, mn in enumerate(self.metric_names):
+                    sys.stdout.write(' {}: {:.4}'.format(mn, metric_vals[m_idx]))
+            sys.stdout.flush()
+
+        # display batch statistics
+        print(
+            '\n' + 'Avg Loss: {:.4}'.format(
+                running_loss / n_batches,
+            )
+        )
+
+        if self.metrics is not None:
+            for m_idx, rm in enumerate(running_metrics):
+                print('Avg {}: {:.4}'.format(
+                    self.metric_names[m_idx], rm / n_batches
+                ))
+
+                if self.tb_writer is not None:
+                    try:
+                        if m_idx == 0:
+                            self.tb_writer.add_scalar(f'Avg Loss/{mode}', running_loss / n_batches, e)
+
+                        self.tb_writer.add_scalar(f'Avg {self.metric_names[m_idx]}/{mode}', rm / n_batches, e)
+                        self.tb_writer.flush()
+
+                    except:
+                        pass
 
         self.losses[mode].append(running_loss / n_batches)
-
-    def init_ddp(self):
-        '''
-        Standard PyTorch setup for distributed dataparallel
-
-        Returns
-        -------
-            None
-        '''
-        os.environ['MASTER_ADDR'] = 'localhost'
-        os.environ['MASTER_PORT'] = '12355'
-
-    def prep_ddp_loaders(self, dataset: Iterable, rank: int, world_size: int):
-        '''
-        Distributes a dataloader across world size.
-
-        Returns
-        -------
-            loader:
-                The dataloader provided to the function with the distributed dataset and sampler
-        '''
-
-        sampler = DistributedSampler(
-            dataset,
-            rank = rank,
-            num_replicas = world_size,
-            shuffle = True,
-            drop_last = True
-        )
-
-        loader = DataLoader(
-            dataset,
-            batch_size = self.batch_size,
-            num_workers = self.num_workers,
-            sampler = sampler,
-            drop_last = True,
-        )
-
-        return loader
-
-    def setup_ddp_modules(self, rank, net):
-
-        print('Splitting data loader ...')
-        # split the dataloader
-        self.train_loader = self.prep_ddp_loaders(self.train_dataset, rank, self.world_size)
-        self.valid_loader = self.prep_ddp_loaders(self.valid_dataset, rank, self.world_size)
-
-        print('Distributing model ...')
-        # wrap the model
-        net = DDP(net.to(rank), device_ids = [rank], output_device = rank)
-
-        # wrap the optimizer
-        optimizer = self.optimizer
-        optimizer.parameters = self.net.parameters
-
-        return net, optimizer
-
 
     def get_loss(self, inputs: torch.Tensor, targets: torch.Tensor):
         '''
